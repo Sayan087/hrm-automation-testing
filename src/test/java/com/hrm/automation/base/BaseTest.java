f5317ff939ae17b340620d16ff40f32339cdd467
Sayan Giri <95565159+Sayan087@users.noreply.github.com>
2026-05-09 08:21:12 +0530

    Update BaseTest.java

--- src/test/java/com/hrm/automation/base/BaseTest.java
+++ src/test/java/com/hrm/automation/base/BaseTest.java
@@ -2,26 +2,35 @@ package com.hrm.automation.base;
 
 import com.hrm.automation.utils.ConfigReader;
 import org.openqa.selenium.WebDriver;
-
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class BaseTest {
+
     public WebDriver driver;
+
     @BeforeMethod
     public void setup() {
 
-        // Selenium Manager handles driver automatically
-        driver = new ChromeDriver();
+        // Use system-installed ChromeDriver (avoid download)
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 
-        driver.manage().window().maximize();
+        // Headless mode for EC2 (no GUI)
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
 
         driver.manage().timeouts()
                 .implicitlyWait(Duration.ofSeconds(5));
+
+        driver.manage().window().maximize();
 
         driver.get(ConfigReader.get("url"));
     }
